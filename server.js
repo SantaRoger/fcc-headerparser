644cf6ae39fb02043ef918c6e735415f25431ad7
Roger Minton <rminton@qinship1.com>
2020-12-01 13:58:09 -0500

    added endpoint /api/whoami

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // server.js
 // where your node app starts
-
+require('dotenv').config();
 // init project
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,12 +19,18 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get('/api/whoami', (req,res) => {
+  res.json({
+    ipaddress: req.connection.remoteAddress,
+    language:  req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
